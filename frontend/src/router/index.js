288bfe16bc7823b2d31a871b4395d50a492782f9
Kio <48633146+Kiomars93@users.noreply.github.com>
2021-12-21 20:31:35 +0100

    Added some basic confirmation page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,13 +3,15 @@ import VueRouter from 'vue-router';
 import Booking from '@/views/Booking/routes.js';
 import TrainDepartures from '@/views/TrainDepartures/routes.js';
 import TestAPI from '@/views/TestAPI/routes.js';
+import Confirmation from '@/views/Confirmation/routes.js';
 
 Vue.use(VueRouter);
 
 const routes = [
 	...Booking,
 	...TrainDepartures,
-	...TestAPI
+	...TestAPI,
+	...Confirmation
 ];
 const router = new VueRouter({
 	mode: 'history',
