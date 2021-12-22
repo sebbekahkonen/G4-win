41e8a2f5fdceae9a82e7a1923379b065273108e1
Jesper Lundbäck <68069660+Jesper1990@users.noreply.github.com>
2021-12-22 10:45:38 +0100

    working on seatbooking

--- frontend/src/views/Seats/Seats.vue
+++ frontend/src/views/Seats/Seats.vue
@@ -11,19 +11,66 @@
 		<v-col align="center">
 			Valt datum: 2021-12-20
 		</v-col>
-		<v-card align="center">
+		<v-card outlined align="center">
 			<v-icon left large @click="decreaseWagons">{{ 'mdi-chevron-left' }}</v-icon>
 			<v-btn id="custom-disabled" text disabled>{{ 'Vagn ' + noOfWagons }}</v-btn>
 			<v-icon right large @click="increaseWagons">{{ 'mdi-chevron-right' }}</v-icon>
 		</v-card>
+		<div class="seat-container">
+			<div v-for="seats in nrOfSeats" :key="seats" class="seat-booking">
+				<v-icon v-model="specificSeat" :color="icon.color" @click="testFunction(seats)">{{ icon.name }}</v-icon>
+			</div>
+		</div>
 	</v-container>
 </template>
 <script>
+import vue from 'vue';
 export default {
 	data: () =>  ({
-		noOfWagons: 1
+		noOfWagons: 1,
+		nrOfSeats: [],
+		specificSeat: '',
+		icon: {
+			name: 'mdi-checkbox-blank-circle-outline',
+			color: 'green'
+		},
+		colors: {
+			green: 'green',
+			red: 'red'
+		}
 	}),
+	created() {
+		vue.nextTick(this.fillSeatArr());
+	},
 	methods: {
+		fillSeatArr() {
+			for(let i=0; i<40; i++) {
+				this.nrOfSeats.push(i);
+			}
+		},
+		testFunction(seats) {
+			this.specificSeat = seats;
+			// console.log(e.target.value);
+			// if(seats) {
+			// 	this.specificSeat = seats; 
+			// }
+			
+			// if(this.specificSeat === seats) {
+			// 	this.icon.color = this.colors.red;
+			// } else if (this.specificSeat !== seats) {
+			// 	this.icon.color = this.colors.green;
+			// }
+
+			console.log(this.specificSeat);
+		},
+		bookSeats() {
+
+			if(this.icon.color === this.colors.green) {
+				this.icon.color = this.colors.red;
+			} else {
+				this.icon.color = this.colors.green;
+			}
+		},
 		increaseWagons() {	
 			if(this.noOfWagons != 7) 
 				this.noOfWagons += 1;		
@@ -39,5 +86,22 @@ export default {
 <style>
 #custom-disabled.v-btn--disabled {
   color: black !important;
+}
+.seat-container {
+	margin-top: 20px;
+	margin-left: -15px;
+	display: grid;
+	grid-template-columns: 12px 50px 12px 12px;
+	grid-template-rows: 40px 10px 40px 10px 40px 10px 40px 10px;
+	column-gap: 10px;
+	row-gap: 20px;
+	justify-content: center;
+}
+.seat-booking {
+	width: fit-content;
+	height: fit-content;
+}
+.seats {
+	cursor: pointer;
 }
 </style>
