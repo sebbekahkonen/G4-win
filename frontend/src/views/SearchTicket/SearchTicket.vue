56a605d366cf797b029b2fa09fd367c01c316799
Jesper Lundbäck <68069660+Jesper1990@users.noreply.github.com>
2022-01-01 16:06:22 +0100

    Design idea for tickets view

--- frontend/src/views/SearchTicket/SearchTicket.vue
+++ frontend/src/views/SearchTicket/SearchTicket.vue
@@ -1,55 +1,70 @@
 <template>
-	<div>
-		<v-card dark height="fit-content" class="justify-center  mt-16 ml-2 mr-2 elevation-16">
-			<v-col class="flex text-center pa-3">
-				<h3>Sök efter din biljett</h3>
-				{{ getAllTickets }}
-			</v-col>
-			<v-text-field v-model="ticketInput" class="pa-10" label="Search.." append-icon="mdi-magnify" @click:append="search" />
+	<v-container class="justify-center">
+		<v-col class="flex text-center pa-3">
+			<h3>Sök efter din biljett</h3>
+			{{ getAllTickets }}
+		</v-col>
+		<v-text-field 
+			v-model="ticketInput"
+			solo
+			dense
+			color="blue"
+			label="Sök biljett"
+			append-icon="mdi-magnify"
+			@click:append="search"
+		/>
+		<v-card>			
+			<v-select 
+				:items="sortArray"
+				label="Sort by"
+				clearable
+				class="pl-2 pr-2"
+			/>			
+			
 			<v-data-table
 				:headers="ticketHeaders"
 				:items="tickets"
 				:single-expand="singleExpand"
 				:expanded.sync="expanded"
-				item-key="name"
+				item-key="name"	
+				mobile-breakpoint="0"		
 				show-expand
-				class="elevation-10 ma-2"
 				@click:row="expandRow"
-				@item-expanded="onExpand"
-				@row-clicked="onExpand"
+				@item-expanded="onExpand"		
+				@row-clicked="onExpand"		
 			>
 				<template v-slot:expanded-item="{ headers }">
 					<td :colspan="headers.length" width="400">
-						<v-col>
-							<v-layout>
+						<v-row justify="space-between">
+							<v-col class="pa-0 pt-4">
 								<h3>Namn: {{ firstAndLast }}</h3>
-							</v-layout>
+							</v-col>
+							<v-col class="pa-0 pt-4">
+								<h3 class="text-right">Platser: {{ seatsBooked }}</h3>
+							</v-col>
+						</v-row>																
+						<v-col class="mt-4">
 							<v-layout>
-								<h3>Platser: {{ seatsBooked }}</h3>
-							</v-layout>
-						</v-col>
-						<v-col>
-							<v-layout>
-								<h4 v-if="btnClicked" class="flex text-center red--text">Är du säker på att du vill avboka?</h4>
+								<h3 v-if="btnClicked" class="flex text-center red--text">Är du säker på att du vill avboka?</h3>
 							</v-layout>
 							<v-layout row wrap class="pa-3">
-								<v-btn class="flex text-left mr-10" @click="cancelBooking">Avboka</v-btn>
-								<v-btn v-if="btnClicked" class="flex text-right mr-5">Ja</v-btn>
-								<v-btn v-if="btnClicked" class="flex text-right">Nej</v-btn>
+								<v-btn v-if="btnClicked === false" class="flex text-left" color="blue darken-1 white--text" @click="cancelBooking">Avboka</v-btn>
+								<v-btn v-if="btnClicked" class="flex text-right mr-5" color="success">Ja</v-btn>
+								<v-btn v-if="btnClicked" class="flex text-right" color="error" @click="btnClicked = false">Nej</v-btn>
 							</v-layout>
 						</v-col>
 					</td>
 				</template>
 			</v-data-table>
 		</v-card>
-	</div>
+	</v-container>
 </template>
 <script>
 import { mapActions, mapGetters } from 'vuex';
 export default {
 	data: () => ({
 		firstAndLast: 'John Doe',
-		seatsBooked: '4 8 2',
+		seatsBooked: '4, 8, 2',
 		ticketInput: '',
 		btnClicked: false,
 		expanded: [],
@@ -58,18 +73,34 @@ export default {
 			{ 
 				text: 'Name',
 				value: 'name'
+			},
+			{
+				text: 'Price',
+				value: 'price'
+			},
+			{ 
+				text: '',
+				value: 'data-table-expand'
 			}
 		],
 		tickets: [
 			{
-				name: 'Biljett 1'
+				name: 'Biljett 1',
+				price: '450kr'
 			},
 			{
-				name: 'Biljett 2'
+				name: 'Biljett 2',
+				price: '1500kr'
 			},
 			{
-				name: 'Biljett 3'
+				name: 'Biljett 3',
+				price: '200kr'
 			}
+		],
+		sortArray: [
+			'Name',
+			'Price',
+			'Date'
 		]
 	}),
 	computed: {
